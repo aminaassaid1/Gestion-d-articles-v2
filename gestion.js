d92650d7d92c290027ae05068b728be456b48405
Amina Assaid <aminaassaid123@gmail.com>
2022-12-12 16:54:46 +0100

    h

--- gestion.js
+++ gestion.js
@@ -1,1 +1,26 @@
-let name = document.getElementById(name)
+let name = document.getElementById('name')
+let price = document.getElementById('price')
+let mark = document.getElementById("mark")
+let date = document.getElementById("date")
+let category = document.getElementById("category")
+let sale = document.getElementById("sale")
+let nameRe = /^[a-z]{1,30}$/i;
+let priceRe = /^\d+$/
+class product {
+    constructor(name , price , mark , date , category , sale ){
+        this.name = name ;
+        this.price = price ;
+        this.mark = mark ;
+        this.date = date ;
+        this.category = category ;
+        this.sale = sale ;
+
+
+    }
+    details(){
+        return 
+        
+    }
+}
+    
+    
